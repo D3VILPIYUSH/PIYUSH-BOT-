d9fdd724f39a1c8eed94ac6152432e30f211c55d
sameerpanthi <75129693+sameerpanthi@users.noreply.github.com>
2021-04-27 13:29:48 +0530

    Update savage_alive.py

--- userbot/plugins/savage_alive.py
+++ userbot/plugins/savage_alive.py
@@ -10,7 +10,7 @@ from userbot.utils import lightning_cmd
 
 LIGHTNING_ALV_IMG = os.environ.get("LIGHTNING_ALV_IMG", None)
 if LIGHTNING_ALV_IMG is None:
-    ALV_LIGHTNING = "https://telegra.ph/file/5a121317fed843d1cd3fc.mp4"
+    ALV_LIGHTNING = "https://telegra.ph/file/dcd029904392b0563d9a9.jpg"
 else:
     ALV_LIGHTNING = LIGHTNING_ALV_IMG
 
@@ -50,20 +50,18 @@ DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else "SAVAGE BOY"
 TG = str(TG_GRUP) if TG_GRUP else "Not  Yet😁😁"
 TG_CHANN = str(TG_CHANNEL) if TG_CHANNEL else "Not Yet😁😁"
 
-
 from userbot import CMD_LIST
-
-pm_caption = "__                       **🔥 𝐒𝐀𝐕𝐀𝐆𝐄_𝐁𝐎𝐓 🔥**  __\n\n"
+               
+pm_caption = "__                       **🔥 𝐃3𝐕𝐈𝐋_𝐁𝐎𝐓 🔥**  __\n\n"
 pm_caption += f"               __↼🄼🄰🅂🅃🄴🅁⇀__\n**      『{DEFAULTUSER}』**\n\n"
 pm_caption += "✘ ΔβØỮŦ Μ¥ Ş¥ŞŦ€Μ ✘\n\n"
-pm_caption += "➾ **𝐓𝐄𝐋𝐄𝐓𝐇𝐎𝐍**       ➣ 𝟏.𝟏𝟕.𝟓\n"
-pm_caption += "➾ **𝐓𝐄𝐀𝐌 𝐆𝐑𝐎𝐔𝐏      ➣ [𝐒𝐀𝐕𝐀𝐆𝐄](https://t.me/joinchat/RPrJW2IU-Uo4MGRl)\n"
-pm_caption += "➾ **𝐒𝐔𝐏𝐏𝐎𝐑𝐓 𝐂𝐇𝐀𝐍𝐍𝐄𝐋 ➣ [𝐉𝐎𝐈𝐍](https://t.me/joinchat/0KCyT0MHyAhmMmRl)\n"
-pm_caption += "➾ **𝐒𝐔𝐏𝐏𝐎𝐑𝐓 𝐆𝐑𝐎𝐔𝐏   ➣ [𝐉𝐎𝐈𝐍](https://t.me/joinchat/qCIk-af6VW1kNDll)\n"
-pm_caption += "➾ **𝐂𝐑𝐄𝐀𝐓𝐎𝐑      ➣ [⚡𝐒𝐀𝐌𝐄𝐄𝐑⚡](@SAMEER_795)\n" 
-                  
+pm_caption += "𝐓𝐄𝐋𝐄𝐓𝐇𝐎𝐍 𝐕𝐄𝐑𝐒𝐈𝐎𝐍 : 1.17.5\n"
+pm_caption += "𝐒𝐔𝐏𝐏𝐎𝐑𝐓 𝐂𝐇𝐀𝐍𝐍𝐄𝐋  : [𝐉𝐎𝐈𝐍](https://t.me/SAVAGE_TECHY)\n"
+pm_caption += "𝐒𝐔𝐏𝐏𝐎𝐑𝐓 𝐆𝐑𝐎𝐔𝐏    : [𝐉𝐎𝐈𝐍](https://t.me/D3VIL_BOT_SUPPORT)\n"
+pm_caption += "𝐓𝐄𝐀𝐌 𝐆𝐑𝐎𝐔𝐏       : [𝐃3𝐕𝐈𝐋](https://t.me/D3VIL_0358)\n\n"
+pm_caption += "𝐂𝐑𝐄𝐀𝐓𝐎𝐑          ➣ [⚡𝐒𝐀𝐌𝐄𝐄𝐑⚡](@SAMEER_795)\n"                   
 pm_caption += " \n"
-pm_caption += "[✨𝔻𝔼ℙ𝕃𝕆𝕐 𝕐𝕆𝕌ℝ 𝕊𝔸𝕍𝔸𝔾𝔼✨](https://github.com/sameerpanthi/SAVAGE-IS-BACK)"
+pm_caption += "[ꀷꏂᖘ꒒ꂦꌩ ꌩꂦꀎꋪ ꂦꅐꈤ ꀷꏂ꒦ꀤ꒒](https:/github.com/sameerpanthi/D3VI-2.0)\n"
 
 @borg.on(lightning_cmd(pattern=r"alive"))
 @borg.on(sudo_cmd(pattern=r"alive", allow_sudo=True))
