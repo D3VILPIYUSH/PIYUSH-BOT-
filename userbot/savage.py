96d1c6e702c873a8f75c3718264a880edca2562b
sameerpanthi <75129693+sameerpanthi@users.noreply.github.com>
2021-04-27 13:32:05 +0530

    Update savage.py

--- userbot/savage.py
+++ userbot/savage.py
@@ -8,15 +8,15 @@ from userbot import ALIVE_NAME
 MASTER = str(ALIVE_NAME) if ALIVE_NAME else "SAVAGE BOY"
 NAME = os.environ.get("BOT_NICK_NAME")
 BOT =  = str(NAME) if NAME else "SAVAGE BOT"
-#op for keinshin
+#op for sameer
 
 SAMEER = "[SAMEER](https://t.me/sameer_795)"
-OP = "[SAVAGE BOT](https://github.com/sameerpanthi/SAVAGE-IS-BACK)"
+OP = "[D3VIL BOT](https://github.com/sameerpanthi/D3VIL-2.0)"
 OKAY = "[SUPPORT GROUP](https://t.me/savage_team_userbot)"
 #itna test h aur aage krte h
 #test successful raha ab aage 
-ALIVE = "SAVAGE BOT IS ON🔥 FIRE 🔥" #make by LEGENDX22 for SAVAGE BOT
-USERBOT = " HELLO MASTER MY NAME IS SAVAGE BOT I AM A BEST USERBOT 💝"
+ALIVE = "D3VIL BOT IS ON🔥 FIRE 🔥" #make by LEGENDX22 for D3VIL BOT
+USERBOT = " HELLO MASTER MY NAME IS D3VIL BOT I AM A BEST USERBOT 💝"
 EMOJI = "⚡"
 #yrr isko apne bot me aply krne se pehle mere se pooch lena ok
 #aur aage add kruga abhi busy okay 🤔
